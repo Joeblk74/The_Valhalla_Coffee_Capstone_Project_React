10fdc4ff79d39f38f235537ac924a4730a60c78d
joeblk74 <joeblk@gmail.com>
2021-11-08 09:23:35 -0900

    Addition of project wireframe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,26 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            coffee: [],
+        };
     }
+componentDidMount(){
+    this.get_All_Coffee();
+}
+
+async get_All_Coffee(){
+    let response = await axios.get('http://127.0.0.1:8000/api/coffee/all/');
+    this.setState({
+        coffee: response.data
+    });
+    console.log(response.data)
+
+}
+
 
     render() {
         return (
